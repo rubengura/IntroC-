c86cb39871909401727ba6c321024075d0db663c
Rubén <ruben.guerrero.ramirez@gmail.com>
2019-09-09 18:33:05 +0200

    Animals cpp improved

--- Animals/Animals.cpp
+++ Animals/Animals.cpp
@@ -9,6 +9,8 @@ private:
 public:
 	//Creating a constructor for the Animal class:
 	Animal() { cout << "Animal created." << endl; };
+	//Creating the copy constructor for the Animal class. It is necessary to define the fields of the class:
+	Animal(const Animal& other) : name(other.name) { cout << "Animal created by copying," << endl; };
 	void setName(string name) { this->name = name; };
 	void speak() const { cout << "My name is: " << name << endl; }
 };
@@ -22,10 +24,15 @@ int main() {
 	// Creating a second Animal object based on the first Animal created avoids using the constructor defined on line 11.
 	// What is happening is that we are using the "Copy constructor".
 	Animal animal2 = animal1;
+	// If no fields are declared in the copy constructor, calling this method would return a blank name.
+	animal2.speak();
 	animal2.setName("Bob");
 	
 	animal1.speak();
 	animal2.speak();
+
+	Animal animal3(animal1);
+	animal3.speak();
 
 	return 0;
 }
